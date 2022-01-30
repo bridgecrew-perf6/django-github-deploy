72459f837902a4718049a454ac626aa7437004c5
ed <edchapman88@gmail.com>
2022-01-30 02:04:21 +0000

    Add Deploy job to workflow.

--- app/hello_django/settings.py
+++ app/hello_django/settings.py
@@ -24,7 +24,11 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DEBUG = int(os.environ.get('DEBUG', default=0))
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = [
+    '143.198.99.0',  # Digital Ocean
+    'localhost',
+    '127.0.0.1'
+]
 
 # Application definition
 
